a8c22e133c95fe6c09ac81711e013de83173bf58
youzaiyouzai666 <1620030379@qq.com>
2018-04-19 11:21:56 +0800

    Initial 拷贝

--- js-base/深浅拷贝/copy.js
+++ js-base/深浅拷贝/copy.js
@@ -6,7 +6,7 @@ function shallowCopy(obj){
     if(typeof obj !== 'object') return;
 
     const newObj = obj instanceof Array ? [] :{};
-    for(var key in obj){
+    for(let key in obj){
         if(obj.hasOwnProperty(key)){
             newObj[key] = obj[key];
         }
@@ -14,10 +14,22 @@ function shallowCopy(obj){
     return newObj;
 }
 
+function deepCopy(obj){
+    if(typeof obj !== 'object') return;
+
+    const newObj = obj instanceof Array ? [] :{};
+
+    for(let key in obj){
+        if(obj.hasOwnProperty(key)){
+            newObj[key] = typeof obj[key] === "object" ? deepCopy(obj[key]) : obj[key];
+        }
+    }
+    return newObj;
+}
 /****************************test************************************/
 ;(function test(){
     const arr = [1,2, {old: 'old'}, ['old']];
-    const arr_new = shallowCopy(arr);
+    const arr_new = deepCopy(arr);
 
     console.log('arr_new',arr_new);
     arr_new[2].old = 'new';
